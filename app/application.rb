83fd65b0a3a8b8523f83fe42d6508005ceab64ec
James Britton <brittonjp@gmail.com>
2019-08-18 15:14:38 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -8,7 +8,7 @@ class Application
     
     if req.path.match(/items/) 
       item_name = req.path.split("/items/")
-      item = @@items
+      item = @@items.find { |item| @@items.name == item_name }
     else
       resp.write "Route not found"
       resp.status = 404
